7e8ff6420bee563a32d065d2e46ff73ea2595f22
Armand Nasseri <aanasseri@ucdavis.edu>
2019-04-28 19:17:26 -0700

    first commit

---
+++ singleServerQ.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include <queue>
+
+#define MAXBUFFER = 100000
+#define ARRIVAL = 0
+#define DEPARTURE = 1
+
+using namespace std;
+
+
+class Event {
+    
+
+    private:
+        int event_time; //time when event occurs
+
+};
+
+// Implement as a double linked list
+struct GlobalEventList {
+
+        void insert();
+        void remove();
+
+};
+
+struct Packet {
+
+};
+
+
+int main() {
+    
+
+
+    return 0;
+}
