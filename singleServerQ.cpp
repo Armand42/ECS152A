1c81879bb57957f3e80c1c3e27eb70044efc36b9
Armand Nasseri <aanasseri@ucdavis.edu>
2019-05-12 18:47:50 -0700

    pretty version

--- singleServerQ.cpp
+++ singleServerQ.cpp
@@ -1,21 +1,25 @@
+// Single Server Queue
+// Yara Fawaz   912243010
+// Armand Nasseri   912679383
+// Jennifer Nguyen  914995612
+
 #include <iostream>
 #include <iomanip>
 #include <queue>
 #include <math.h>
 
-#define MAXBUFFER -1
+#define MAXBUFFER 30
 #define ARRIVAL 0
 #define DEPARTURE 1
 #define SUCCESS true
 #define FAILURE false
-// How to increment time
-//
-using namespace std;
 
+using namespace std;
+// Event Node
 struct Event
 {
   public:
-    double eventTime; //time when event occurs
+    double eventTime;
     bool eventType;
     Event *next;
     Event *prev;
@@ -24,11 +28,10 @@ struct Event
 // Global Event list: Implemented as a double linked list
 class GEL
 {
-    public:
+  public:
     Event *head;
     Event *tail;
     GEL();
-    bool insertAtTail(Event *newEvent);
     bool insertEvent(Event *newEvent);
     Event *removeFirstEvent();
     void printGEL();
@@ -41,41 +44,13 @@ struct Packet
     double serviceTime;
     Packet *next;
 };
-/*
-class FIFOQueue
-{
-    public:
-    Packet *front;
-    Packet *back;
-    uint length;
-
-    FIFOQueue();
-    bool insertPacket(Packet *newPacket);
-    Packet *removePacket();
-    bool isQueueFull();
-    void printQueue();
-};
-*/
-
 
 GEL::GEL()
 {
     head = tail = NULL;
 }
 
-bool GEL::insertAtTail(Event *newEvent)
-{
-    if (head == NULL && tail == NULL)
-    {
-        head = tail = newEvent;
-        return SUCCESS;
-    }
-    tail->next = newEvent;
-    newEvent->prev = tail;
-    tail = newEvent;
-    return SUCCESS;
-}
-
+// Insert an event at the proper location of the linked list
 bool GEL::insertEvent(Event *newEvent)
 {
     if (head == NULL && tail == NULL)
@@ -84,11 +59,11 @@ bool GEL::insertEvent(Event *newEvent)
         return SUCCESS;
     }
     Event *index = head;
-    while(index != NULL && index->eventTime < newEvent->eventTime)
+    while (index != NULL && index->eventTime < newEvent->eventTime)
     {
         index = index->next;
     }
-    if(index == head)
+    if (index == head)
     {
         newEvent->next = head;
         head->prev = newEvent;
@@ -107,15 +82,14 @@ bool GEL::insertEvent(Event *newEvent)
         index->prev->next = newEvent;
         index->prev = newEvent;
     }
-    
 
     return SUCCESS;
 }
-
+// Extracts the first element of the global event list
 Event *GEL::removeFirstEvent()
 {
     Event *first = head;
-    if (head == tail) 
+    if (head == tail)
     {
         head = tail = NULL;
         // printGEL();
@@ -124,29 +98,29 @@ Event *GEL::removeFirstEvent()
 
     head = first->next;
     head->prev = NULL;
-    
+
     return first;
 }
-
+// Helper function to print all events in our list
 void GEL::printGEL()
 {
     Event *current = head;
-    cout<<"\nGEL: ";
-    int i = 0;
-    while(current!=NULL)
+    cout << "\nGEL: ";
+
+    while (current != NULL)
     {
 
-        printf("%s EventTime: %f -> ",current->eventType ==ARRIVAL?"Arrival":"Departure",current->eventTime);
+        printf("%s EventTime: %f -> ", current->eventType == ARRIVAL ? "Arrival" : "Departure", current->eventTime);
         current = current->next;
     }
 }
-
+// Helper function to determine the current state of the packet
 bool GEL::isTransmittingPacket()
 {
     Event *current = head;
-    while(current != NULL)
+    while (current != NULL)
     {
-        if(current->eventType == DEPARTURE)
+        if (current->eventType == DEPARTURE)
             return true;
 
         current = current->next;
@@ -154,156 +128,107 @@ bool GEL::isTransmittingPacket()
     return false;
 }
 
-/*FIFOQueue::FIFOQueue()
+// Helper function to print all Queue elements
+void printQueue(queue<Packet *> q)
 {
-    front = back = NULL;
-    length = 0;
-}
 
-bool FIFOQueue::insertPacket(Packet *newPacket)
-{
-    // first packet in the list
-    if(front == NULL)
-        front = back = newPacket;
-    // other packets
-    else{
-        back->next = newPacket;
-        back = newPacket;
+    cout << "Queue: ";
+    while (!q.empty())
+    {
+        printf("%f ->", q.front()->serviceTime);
+        q.pop();
     }
-    length++;
-    // cout << "LENGTH: " << length << endl;
-    return SUCCESS;
+    cout << endl;
 }
-
-Packet *FIFOQueue::removePacket()
-{
-    if (front == NULL)
-        return NULL;
-    Packet *first = front;
-    front = first->next;
-    length--;
-     cout << "REMOVE LENGTH: " << length << endl;
-    return first;
-}
-
-bool FIFOQueue::isQueueFull()
-{
-    if(MAXBUFFER == -1)
-        return false;
-    if (length >= MAXBUFFER)
-        return true;
-    return false;
-}
-
-void FIFOQueue::printQueue()
-{
-    Packet *current = front;
-}
-*/
-void printQueue(queue<Packet*> q)
-{
-	//printing content of queue 
-    cout<<"Queue: ";
-	while (!q.empty()){
-		printf("%f ->",q.front()->serviceTime);
-		q.pop();
-	}
-	cout<<endl;
-}
-double negativeExponentiallyDistributedTime(double rate) 
+// Function provided by assignment
+double negativeExponentiallyDistributedTime(double rate)
 {
     double u;
     u = drand48();
-    return ((-1/rate) * log(1-u));
+    return ((-1 / rate) * log(1 - u));
 }
 
 int main()
 {
-	queue<Packet*> queue;
-    cout << "STATISTICS\n";
+    queue<Packet *> queue;
+    cout <<"***STATISTICS***\n";
     cout << endl;
     double arrivalRates[7] = {0.1, 0.2, 0.4, 0.5, 0.6, 0.8, 0.9};
-    for(int i = 0; i < 7; i++)
+    for (int i = 0; i < 7; i++)
     {
-        
+
         //Time and counters for statistics.
-        double currentTime = 0, serverBusyTime = 0, totalNumberOfPackets = 0, droppedPackets = 0, intervalTime = 0; 
+        double currentTime = 0, serverBusyTime = 0, totalNumberOfPackets = 0, droppedPackets = 0, intervalTime = 0;
         //Rates
-        double serviceRate = 1; //mu
+        double serviceRate = 1;               //mu
         double arrivalRate = arrivalRates[i]; //lamda
 
-        //Data Structure
+        //Data Structure Init
         GEL gel = GEL();
         Event event = {negativeExponentiallyDistributedTime(arrivalRate) + currentTime, ARRIVAL, NULL, NULL};
         gel.insertEvent(&event);
-        
-        for (int i = 0; i < 100000; i++) {
-        //    gel.printGEL();
 
+        for (int i = 0; i < 100000; i++)
+        {
             Event *currentEvent = gel.removeFirstEvent();
-            intervalTime =  currentEvent-> eventTime - currentTime;
-            // cout<<"\t \t interval time: "<< intervalTime << "\t current time: "<< currentTime <<endl;
+            intervalTime = currentEvent->eventTime - currentTime;
             currentTime = currentEvent->eventTime;
-            if (currentEvent->eventType == ARRIVAL) {
+            if (currentEvent->eventType == ARRIVAL)
+            {
                 // Generating the next arrival
-                double arrival =negativeExponentiallyDistributedTime(arrivalRate), departure =  negativeExponentiallyDistributedTime(serviceRate);
-               // printf("\tarrival: %f        \t departure: %f \n", arrival, departure);
-                Event newArrival = { arrival + currentTime, ARRIVAL, NULL, NULL};
+                double arrival = negativeExponentiallyDistributedTime(arrivalRate), departure = negativeExponentiallyDistributedTime(serviceRate);
+
+                Event newArrival = {arrival + currentTime, ARRIVAL, NULL, NULL};
                 gel.insertEvent(&newArrival);
                 Packet packet = {departure, NULL};
-                
+
                 // Processing the Arrival Event
-                if (!gel.isTransmittingPacket()) 
+                if (!gel.isTransmittingPacket())
                 {
                     double serviceTime = packet.serviceTime;
                     Event departureEvent = {(currentTime + serviceTime), DEPARTURE, NULL, NULL};
                     gel.insertEvent(&departureEvent);
-
                 }
-                else {
-           //         cout<<"INSERT PACKET\n";
-                    if (queue.size() >= MAXBUFFER)  
+                else
+                {
+
+                    if (queue.size() >= MAXBUFFER)
                     {
-                        droppedPackets++;  
-                    } 
-                    else 
+                        droppedPackets++;
+                    }
+                    else
                     {
                         queue.push(&packet);
-                    
                     }
                     totalNumberOfPackets += intervalTime * queue.size();
-                    serverBusyTime+= intervalTime;
+                    serverBusyTime += intervalTime;
                 }
             }
-            // else departure event
-            else 
+            // Processing the Departure Event
+            else
             {
                 if (queue.size() != 0)
                 {
                     Packet *departurePacket = queue.front();
-		    queue.pop();
+                    queue.pop();
                     double serviceTime = departurePacket->serviceTime;
-                    // printf(f"interval time %f\n", intervalTime);
-		    Event departureEvent = {currentTime + serviceTime, DEPARTURE, NULL, NULL};
+
+                    Event departureEvent = {currentTime + serviceTime, DEPARTURE, NULL, NULL};
                     gel.insertEvent(&departureEvent);
-                    
+
                     totalNumberOfPackets += intervalTime * queue.size();
-                    serverBusyTime+= intervalTime;
+                    serverBusyTime += intervalTime;
                 }
             }
             //printQueue(queue);
-        
         }
-        // cout << endl;
-        // cout << "Arrival Rate " << arrivalRate << endl;
-        // cout << "UTILIZATION: "
-        cout << "ARRIVAL RATE: " << arrivalRate<< "\t";
-        printf("Utilization: %f \tMean Queue Length: %f \tNumber of Packets Dropped: %f \n", ((double)serverBusyTime)/currentTime, ((double)totalNumberOfPackets)/currentTime, droppedPackets);
+        
+        cout << "ARRIVAL RATE: " << arrivalRate << "\t";
+        printf("Utilization: %f \tMean Queue Length: %f \tNumber of Packets Dropped: %f \n", ((double)serverBusyTime) / currentTime, ((double)totalNumberOfPackets) / currentTime, droppedPackets);
         //cout << "TOTAL NUMBER OF PACKETS: " << totalNumberOfPackets << endl;
         //cout << endl;
         //cout << "TOTAL NUMBER OF PACKETS: " << totalNumberOfPackets << endl;
-        
-    }    
+    }
 
     return 0;
 }
